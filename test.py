79e6230104e7c57d271e611d54385eb0c8ba7bb0
GNnight <79556322+GNLX@users.noreply.github.com>
2022-01-19 13:04:23 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,14 @@ print("tencent QQ   2680127796 ")
 print(" thanks ")
 
 
+# douyin_xgorgon、All versions are available、抖音算法
+
+# xgorgon安卓0401-0404-0408
+
+# x_Tyhon、x_Argus、X_Ladon、X_Log
+
+# deviced-register 设备注册、激活、授权。
+
+# feed-protobuf 信息流
+
+# live-wss-pro 直播间数据采集
